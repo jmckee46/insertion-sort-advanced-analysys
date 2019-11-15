ce1281b01590767a88af142c4eb11cf90da361e0
John McKee <jmckee3@mac.com>
2019-11-14 20:02:04 -0800

    working with debug printing

--- insertion-sort-advanced-analysis.go
+++ insertion-sort-advanced-analysis.go
@@ -1,6 +1,37 @@
 package insertionSortAdvancedAnalysis
 
-func insertionSort(arr []int32) int32 {
+import "fmt"
 
-	return 0
+func insertionSort(arr []int32) int32 {
+	fmt.Println("before sort arr:", arr)
+
+	sliceLength := int32(len(arr))
+	var shifts int32
+	var shifted bool
+
+	for {
+		shifted = false
+		for i := int32(0); i < sliceLength-1; i++ {
+			if arr[i] > arr[i+1] {
+				shifted = true
+				switchPositions(arr, i, i+1)
+				shifts++
+				fmt.Println("after switch arr:", arr)
+			}
+		}
+		if !shifted {
+			break
+		}
+	}
+
+	fmt.Println("after sort arr:", arr)
+	fmt.Println("shifts:", shifts)
+	return shifts
+}
+
+func switchPositions(arr []int32, pos1 int32, pos2 int32) {
+	var temp int32
+	temp = arr[pos1]
+	arr[pos1] = arr[pos2]
+	arr[pos2] = temp
 }
--- spec-insert-sort-adv-analysis_test.go
+++ spec-insert-sort-adv-analysis_test.go
@@ -5,4 +5,25 @@ import (
 )
 
 func TestInsertionSort(t *testing.T) {
+
+	slice := []int32{1, 1, 1, 2, 2}
+
+	shifts := insertionSort(slice)
+	if shifts != 0 {
+		t.Errorf("got %d shifts instead of 0", shifts)
+	}
+
+	slice = []int32{2, 1, 3, 1, 2}
+
+	shifts = insertionSort(slice)
+	if shifts != 4 {
+		t.Errorf("got %d shifts instead of 4", shifts)
+	}
+
+	slice = []int32{4, 3, 2, 1}
+
+	shifts = insertionSort(slice)
+	if shifts != 6 {
+		t.Errorf("got %d shifts instead of 6", shifts)
+	}
 }
